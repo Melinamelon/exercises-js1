9bbbd1de3dc17d9c82d184e69aaa321114650541
Melina <loserbaby@hotmail.es>
2021-05-10 22:51:13 +0200

    solo repaso

--- week-1/InClass/exercise-I.js
+++ week-1/InClass/exercise-I.js
@@ -5,10 +5,10 @@ function getAgeInDays(age) {
   function createGreeting(name, age) {
     const ageInDays = getAgeInDays(age);
     const message =
-      "My Name is " + name + " and I was born over " + ageInDays + " days ago!";
+      "My name is " + name + " and I was born over " + ageInDays + " days ago!";
     return message;
   }
 
-  let message = createGreeting(melina, 31);
+  let message = createGreeting("Melina", 31);
 
   console.log(message)
