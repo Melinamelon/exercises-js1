fbbb061349939e4dcf814c989d69c2c2866efebb
Melina <loserbaby@hotmail.es>
2021-05-10 10:28:44 +0200

    retoques

--- week-1/InClass/exercise-C.js
+++ week-1/InClass/exercise-C.js
@@ -1,4 +1,4 @@
 let greeting = "Hola, soy Meli";
 console.log(greeting);
-console.log(greeting);
-console.log(greeting);
+console.log(greeting + " lol");
+console.log("Hola soy un string " + greeting);
--- week-1/InClass/exercise-H.js
+++ week-1/InClass/exercise-H.js
@@ -5,8 +5,8 @@ let in4 = 2;
 
 
 //this function has 2 lines of a song and 2 numbers supposed to be add
-function exerH(in1, in2, in3, in4) {
-  return in1 + " " + in3 + in4 + " " + in2;
+function exerH(asa, ese, isi, oso) {
+  return asa + " " + ese + isi + " " + oso;
 }
 
 const result = exerH(in1, in2, in3, in4);
@@ -15,3 +15,14 @@ console.log(result)
 
 //the difference between return and console.log is that return returns execution to the caller with optional result, and console.log, logs out information in console.
 //actually theres nothing in common between them,  console.log will not influence the flow in our code.
+
+
+// let prota = "Harry";
+// let bestie = "Ron";
+// let villian = "Voldemort";
+
+let prota = "Jack Sparrow";
+let bestie = "Will Turner";
+let villian = "Comodoro";
+
+console.log(prota + " con la ayuda de " + bestie + " ha vencido a " + villian)
