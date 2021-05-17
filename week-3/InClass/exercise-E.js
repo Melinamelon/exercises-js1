e5dff88051f6df912da3af1a2484d5c8de20eeee
Melina <loserbaby@hotmail.es>
2021-05-17 23:47:57 +0200

    hice cambios

--- week-3/InClass/exercise-E.js
+++ week-3/InClass/exercise-E.js
@@ -1,21 +1,20 @@
 // Create a function that takes a birthYear, and returns the age of someone
 // You are given an array with year that 7 people were born,
-let bornYear;
 
-bornYear = [1964, 2008, 1999, 2005, 1978, 1985, 1919];
 
-function getAge() {
-    let arr2 = [];
-    index = 0;
-    for (2021 - x in arr[x]) {
-        arr2[index] = arr[x];
-        2021 - bornYear[x]
-    }
-    return arr2;
+
+
+let years = [1964, 2008, 1990, 2005, 1978, 1985, 1919];
+let date = newDate().getFullYear();
+
+function birthYear(year){
+    return date - year;
 }
 
+let edad = years.map(birthYear);
 
-
+console.log(edad);
+console.log(years);
 
 // Take this array and create another array containing their ages.
 // console.log the birth years array
