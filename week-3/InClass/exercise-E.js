91e383dc6adf4f89c5f7cb87205ecd5bcd7fd393
Melina <loserbaby@hotmail.es>
2021-05-18 01:39:37 +0200

    hice cambios

--- week-3/InClass/exercise-E.js
+++ week-3/InClass/exercise-E.js
@@ -1,11 +1,12 @@
 // Create a function that takes a birthYear, and returns the age of someone
 // You are given an array with year that 7 people were born,
-
+//Date() constructor
+//Creates a JavaScript Date instance that represents a single moment in time in a platform-independent format.Date objects contain a Number that represents milliseconds since 1 January 1970 UTC.
 
 
 
 let years = [1964, 2008, 1990, 2005, 1978, 1985, 1919];
-let date = newDate().getFullYear();
+let date = new Date().getFullYear();
 
 function birthYear(year){
     return date - year;
---
+++ week-3/InClass/exercise-F.js
@@ -0,0 +1,6 @@
+let arr = [1964, 2008, 1999, 2005, 1978, 1985, 1919];
+
+
+arr.forEach(function (arr) {
+    console.log(2021 - arr);
+  });
