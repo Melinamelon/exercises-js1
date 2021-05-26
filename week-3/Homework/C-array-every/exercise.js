c9023a5d6c82559c6ec56a501127c7165fda7349
Melina <loserbaby@hotmail.es>
2021-05-26 22:36:30 +0200

    todos empezados!

--- week-3/Homework/C-array-every/exercise.js
+++ week-3/Homework/C-array-every/exercise.js
@@ -1,11 +1,16 @@
-/*
+/* :c not working
    This program should check if the array `group` contains only students
 */
 
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents; // complete this statement
+var groupIsOnlyStudents = group.every(studentChecker)
+
+function studentChecker(name) {
+  return students.includes(name);
+
+}; // complete this statement
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
@@ -14,5 +19,14 @@ if (groupIsOnlyStudents) {
 }
 
 /* EXPECTED RESULT */
-
 // The group does not contain only students
+
+
+var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley"];
+function isAboveThreshold(name) {
+  return name.length > 3;
+}
+
+var studentNameLength = students.every(isAboveThreshold);
+
+console.log(studentNameLength);
